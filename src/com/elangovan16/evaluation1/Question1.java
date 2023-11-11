20816eb120bcf02e5db23fffd8f7ace05d23f34c
Elangovan16 <pmeelangovanm@gmail.com>
2023-11-11 19:15:58 +0530

    fix-3 adding space

--- src/com/elangovan16/evaluation1/Question1.java
+++ src/com/elangovan16/evaluation1/Question1.java
@@ -1,49 +1,54 @@
 package com.elangovan16.evaluation1;
+
 import java.util.Scanner;
+
 public class Question1 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Number : ");
-        int n=sc.nextInt();
-        int[][] mat=getTriangleSpiral(n);
-        printMatrix(mat);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number : ");
+		int n = sc.nextInt();
+		int[][] mat = getTriangleSpiral(n);
+		printMatrix(mat);
+	}
 
-    private static int[][] getTriangleSpiral(int n) {
-        int[][] mat=new int[n][n];
-        int count=1;
-        int left=0,right=n-1;
-        int top=0,bottom=n-1;
-        while(left<=right&&top<=bottom) {
-            for (int i = top; i <= bottom; i++) {
-                mat[i][i + top] = count++;
-            }
-            left++;
-            bottom--;
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    mat[i][right] = count++;
-                }
-                right--;
-            }
-            bottom--;
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    mat[top][i] = count++;
-                }
-                left++;
-                top++;
-            }
-        }
-        return mat;
-    }
+	private static int[][] getTriangleSpiral(int n) {
+		int[][] mat = new int[n][n];
+		int count = 1;
+		int left = 0, right = n - 1;
+		int top = 0, bottom = n - 1;
+		while (left <= right && top <= bottom) {
+			for (int i = top; i <= bottom; i++) {
+				mat[i][i + top] = count++;
+			}
+			left++;
+			bottom--;
+			if (left <= right) {
+				for (int i = bottom; i >= top; i--) {
+					mat[i][right] = count++;
+				}
+				right--;
+			}
+			bottom--;
+			if (top <= bottom) {
+				for (int i = right; i >= left; i--) {
+					mat[top][i] = count++;
+				}
+				left++;
+				top++;
+			}
+		}
+		return mat;
+	}
 
-    private static void printMatrix(int[][] mat) {
-        for(int[] i:mat){
-            for(int j:i){
-                System.out.printf("%2d ",j);
-            }
-            System.out.println();
-        }
-    }
+	private static void printMatrix(int[][] mat) {
+		for (int[] i : mat) {
+			for (int j : i) {
+				if (j == 0)
+					System.out.printf("   ");
+				else
+					System.out.printf("%2d ", j);
+			}
+			System.out.println();
+		}
+	}
 }
